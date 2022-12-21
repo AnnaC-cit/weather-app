db31edf70ef54f639b4f47ffbbedd45b73632a03
CIANDT\annac <annac@ciandt.com>
2022-12-21 00:05:57 -0300

    Separar a responsabilidade das controllers

--- weather/src/main/kotlin/com/api/weather/HealthControlle.kt
+++ weather/src/main/kotlin/com/api/weather/HealthControlle.kt
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping
-class WeatherController {
+class HealthControlle {
 
-    @GetMapping("/clima")
-    fun clime(location: String): ResponseEntity<Any> {
-        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build()
+    @GetMapping("/health")
+    fun health(): ResponseEntity<HttpStatusCode> {
+        return ResponseEntity.status(HttpStatus.OK).build()
     }
 }
